0dbac2e29e5bd2460c8742f78a1d7dca8787d559
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-10-22 15:27:01 -0500

    Implemented projects CRUD

--- roadmapapi/serializers.py
+++ roadmapapi/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
+# *** Users ***
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+# *** Projects ***
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'description', 'owner', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
