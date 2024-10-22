d5cb629c2707a69adb80170d3daa3d0fe9bfd62c
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-10-22 16:03:08 -0500

    Fixed the import in the serializer.py

--- roadmapapi/serializers.py
+++ roadmapapi/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from roadmapapi.models import Project
 
 # *** Users ***
 class UserSerializer(serializers.ModelSerializer):
