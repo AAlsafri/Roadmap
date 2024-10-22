a683c93dd71981d26ea44f3dfc8a75d829f899b6
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-10-22 11:59:40 -0500

    Auth users full CRUD implemented.

--- roadmapproject/urls.py
+++ roadmapproject/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from roadmapapi.views.users import UserViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
+    path('register', UserViewSet.as_view({'post': 'register_account'}), name='register'),
 
+    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'register_account'})),
+    path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+]
