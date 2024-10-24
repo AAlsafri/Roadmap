e2e4fa758b18aa865549b1cac7f7f2f1f1a2f4ef
Abdul Alsafri <abdulalsafri1@gmail.com>
2024-10-24 11:04:03 -0500

    Milestones CRUD operations.

---
+++ roadmapapi/views/__init__.py
@@ -0,0 +1,4 @@
+from .users import UserViewSet
+from .projects import ProjectViewSet
+from .assignments import AssignmentViewSet
+from .milestones import MilestoneViewSet
--- roadmapproject/urls.py
+++ roadmapproject/urls.py
@@ -4,10 +4,14 @@ from rest_framework import routers
 from roadmapapi.views.users import UserViewSet
 from roadmapapi.views.projects import ProjectViewSet
 from roadmapapi.views.assignments import AssignmentViewSet
+from roadmapapi.views.milestones import MilestoneViewSet
+
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'projects', ProjectViewSet, basename='project')
+router.register(r'assignments', AssignmentViewSet, basename='assignment')
+router.register(r'milestones', MilestoneViewSet, basename='milestone')  
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -22,4 +26,7 @@ urlpatterns = [
 
     path('assignments/', AssignmentViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('assignments/<int:pk>/', AssignmentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+
+    path('milestones/', MilestoneViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('milestones/<int:pk>/', MilestoneViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
